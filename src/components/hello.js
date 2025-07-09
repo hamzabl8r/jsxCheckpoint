47902ce59f5f3a9545dc6f56dd8cf23187a1e957
Hamza Beji <hamzabeji001@gmail.com>
2025-07-09 17:20:11 +0100

    Update hello.js

--- src/components/hello.js
+++ src/components/hello.js
@@ -2,7 +2,8 @@
 var firstName = prompt("Entre your Name : ")
 export default function Hello() {
     if(firstName){
-    return(<h1>Welcome {firstName} to our store</h1>)
+    return(<h1>Hello  {firstName} </h1>)
     }
+    return (<h1>Hello there </h1>)
     
 }
